2b76e8c5e44a795058f3e511dae7028de80b2d75
Krvell <fridrich35@seznam.cz>
2024-02-20 12:39:31 +0100

    drobne upravy

--- main.js
+++ main.js
@@ -12,12 +12,12 @@ function makeAPIRequest(url) {
     });
 }
 
-// Fetch and display random recipes every time the user visits
+// random recipes every time the user visits
 /* document.addEventListener('DOMContentLoaded', function () {
   makeAPIRequest(`https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=3`);
 }); */
 
-// Function to display the API response
+//function for recipe cards
 function displayResults(data) {
   const resultsContainer = document.getElementById('results');
   resultsContainer.innerHTML = '';
@@ -29,7 +29,7 @@ function displayResults(data) {
     recipesArray = data.results;
   } else {
     console.error('Error: Invalid or missing data in the API response');
-    return; // Exit the function if the data structure is not as expected
+    return;
   }
 
   if (recipesArray && recipesArray.length > 0) {
@@ -49,7 +49,6 @@ function displayResults(data) {
 
       const details = document.createElement('p');
       details.classList.add('recipe-details');
-      //details.id = recipe.id;
 
       card.appendChild(image);
       card.appendChild(title);
@@ -67,15 +66,14 @@ function displayResults(data) {
 const searchBar = document.getElementById('searchBar');
 const suggestionsList = document.getElementById('suggestionsList');
 
-// Function to fetch recipe suggestions //funguje
+// fetch recipe suggestions //funguje
 async function fetchSuggestions(query) {
   const response = await fetch(`https://api.spoonacular.com/recipes/autocomplete?apiKey=${apiKey}&query=${query}`);
   const data = await response.json();
   return data; //odstraneno .suggestions
 }
 
-// Function to display suggestions in the dropdown menu
-// Function to display suggestions in the dropdown menu
+// dropdown menu
 function displaySuggestions(suggestions) {
   // Check if the suggestions array is empty
   if (suggestions && suggestions.length > 0) {
@@ -87,18 +85,17 @@ function displaySuggestions(suggestions) {
       listItem.addEventListener('click', () => {
         searchBar.value = suggestion.title;
         suggestionsList.innerHTML = '';
-        // Initiate the search for the selected recipe
+
         searchRecipe(suggestion.title);
       });
       suggestionsList.appendChild(listItem); //zkontrolovat!!
     });
   } else {
-    // If suggestions array is empty, display a message to the user
     suggestionsList.innerHTML = '<li>No suggestions found.</li>';
   }
 }
 
-// Function to handle user input in the search field
+// vyhledavani u dropdown menu
 searchBar.addEventListener('input', async () => {
   const query = searchBar.value.trim();
   if (query.length > 0) {
@@ -109,13 +106,11 @@ searchBar.addEventListener('input', async () => {
   }
 });
 
-// Function to search for a recipe
+
 function searchRecipe(query) {
-  // Perform the recipe search based on the selected suggestion
 
   if (query) {
     const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`;
-    // Fetch and display recipes based on the user's search query
     makeAPIRequest(url);
     // smazani textu po odeslani requestu
     searchBar.value = "";
@@ -132,9 +127,7 @@ button.addEventListener('click', () => {
 
   if (query) {
     const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${query}`;
-    // Fetch and display recipes based on the user's search query
     makeAPIRequest(url);
-    // smazani textu po odeslani requestu
     searchBar.value = "";
   } else {
     alert('Please enter a search query.');
@@ -147,13 +140,12 @@ resultsContainer.addEventListener('click', function (event) {
   const clickedElement = event.target;
   const recipeCard = clickedElement.closest('.recipe-card');
 
-  // Check if a recipe card is clicked
   if (recipeCard) {
     const recipeId = recipeCard.getAttribute('id');
     const title = recipeCard.querySelector('.recipe-title').textContent; //nove
     const image = recipeCard.querySelector('.recipe-image').src;
 
-    // Fetch additional details for the clicked recipe
+    // odkaz na detaily receptu
     const detailsUrl = `recipeDetails.html?recipeId=${recipeId}`;
 
     console.log(detailsUrl);
@@ -162,7 +154,6 @@ resultsContainer.addEventListener('click', function (event) {
     fetch(detailsUrl)
       .then(response => response.json())
       .then(recipeDetails => {
-        // Display the details in the recipe details container
         displayRecipeDetails(recipeDetails);
         console.log(recipeDetails);
       })
@@ -172,12 +163,11 @@ resultsContainer.addEventListener('click', function (event) {
   }
 });
 
-
+// zobrazeni receptu
 function displayRecipeDetails(recipeDetails) {
   const recipeDetailsContainer = document.getElementById('recipeDetailsContainer');
   recipeDetailsContainer.innerHTML = '';
 
-  // Create elements to display details (title, image, ingredients, instructions, etc.)
   const title = document.createElement('h2');
   title.textContent = recipeDetails.title;
 
@@ -185,51 +175,42 @@ function displayRecipeDetails(recipeDetails) {
   image.src = recipeDetails.image;
   image.alt = recipeDetails.title;
 
-  // Add more elements for other details (ingredients, instructions, etc.)
-
-  // Append elements to the recipe details container
   recipeDetailsContainer.appendChild(title);
   recipeDetailsContainer.appendChild(image);
-  // Append other details here...
+
 }
 
 const btnMainCourse = document.getElementById('btnMainCourse');
 btnMainCourse.addEventListener('click', () => {
-  // Fetch recipes for the Side Dishes category
   fetchRecipes('main course');
 });
 
 const btnSideDish = document.getElementById('btnSideDish');
 btnSideDish.addEventListener('click', () => {
-  // Fetch recipes for the Side Dishes category
   fetchRecipes('side dish');
 });
 const btnSoup = document.getElementById('btnSoup');
 btnSoup.addEventListener('click', () => {
-  // Fetch recipes for the Side Dishes category
   fetchRecipes('soup');
 });
 const btnSauce = document.getElementById('btnSauce');
 btnSauce.addEventListener('click', () => {
-  // Fetch recipes for the Side Dishes category
   fetchRecipes('sauce');
 });
 const btnSalad = document.getElementById('btnSalad');
 btnSalad.addEventListener('click', () => {
-  // Fetch recipes for the Side Dishes category
   fetchRecipes('salad');
 });
 const btnBread = document.getElementById('btnBread');
 btnBread.addEventListener('click', () => {
-  // Fetch recipes for the Side Dishes category
   fetchRecipes('bread');
 });
 const btnDessert = document.getElementById('btnDessert');
 btnDessert.addEventListener('click', () => {
-  // Fetch recipes for the Side Dishes category
   fetchRecipes('dessert');
 });
 
+// fetch typy jidel deezert, salát atd.
 function fetchRecipes(type) {
   makeAPIRequest(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&type=${type}`)
   
@@ -238,18 +219,19 @@ function fetchRecipes(type) {
 //vetsi navbar pro filtry vyhledavani
 document.addEventListener('DOMContentLoaded', function() {
   const complexSearch = document.getElementById('complexSearch');
-  const filterCheckboxes = document.getElementById('filterCheckboxes');
+  const moreOptionsSearch = document.getElementById('moreOptionsSearch');
 
   complexSearch.addEventListener('click', function() {
-    // Toggle the visibility of the filter checkboxes
-    if (filterCheckboxes.style.display === 'none') {
-      filterCheckboxes.style.display = 'block';
-      // Adjust the height of the navbar when the checkboxes are visible
-      document.getElementById('navbarId').style.height = '15vw';
+    if (moreOptionsSearch.style.display === 'none') {
+      moreOptionsSearch.style.display = 'block';
+      
+      moreOptionsSearch.style.transition ='transform 5s ease';
+      moreOptionsSearch.style.transform = 'translateX(0)';
     } else {
-      filterCheckboxes.style.display = 'none';
-      // Reset the height of the navbar to its original value
-      document.getElementById('navbarId').style.height = '7vw'; // Adjust the height value as needed
+      moreOptionsSearch.style.transition ='transform 5s ease';
+      
+      moreOptionsSearch.style.transform = 'translateX(-100%)';
+      moreOptionsSearch.style.display = 'none';
     }
   });
 });
